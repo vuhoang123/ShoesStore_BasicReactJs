76db0e23cd5e4f7d1cf60a1237155098b13385e7
vuhoang123 <hoanganhvu741852963@gmail.com>
2022-07-21 14:28:35 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./ex4/Home";
+import ChangeGlasses from "./HomeWork2_reactJs/ChangeGlasses";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      asdjlkasjdlkasjdlkadsjdlasjklsjk
+      <Footer /> */}
+      {/* <Home /> */}
+      {/* <DemoState /> */}
+      {/* <ChangeCar /> */}
+      <ChangeGlasses />
+    </div>
+  );
+}
+
+export default App;
